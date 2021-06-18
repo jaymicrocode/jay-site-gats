f343507e2e254560ed09e153762d96068472a571
jay <jaycode128@gmail.com>
2021-06-18 17:26:37 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "jay site gats",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+  ],
+};
